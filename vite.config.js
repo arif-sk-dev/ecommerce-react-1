1bde4c54c4112725416055578e2a163ad55e3797
arif-sk-dev <arifsk.dev@gmail.com>
2025-10-24 16:48:54 +0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'gh-pages'
+
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/ecommerce-react-1/",
+  base: isGitHubPages ? '/ecommerce-react-1/' : '/',
   plugins: [
     tailwindcss(),
     react()
